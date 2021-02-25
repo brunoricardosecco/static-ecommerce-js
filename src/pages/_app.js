2751eeb7edc7276ec5aca391d55d3246154b746d
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2021-02-24 22:28:44 -0300

    fix: ajust app page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import GlobalStyle from '../styles/global';
+import theme from '../styles/theme';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
+
+export default MyApp;
